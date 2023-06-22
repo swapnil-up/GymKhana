7499999b2462ea2b4de83393160636e6db92b5fd
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-06-22 15:47:12 +0545

    Didn't use API. Had problem with subtracting a date for the month. Added colors for present and absent. and a tracker for streak and total. the streak seems to not work and the red doesn't stop the streak

--- app/src/main/java/com/example/gymkhana/AttendanceActivity.kt
+++ app/src/main/java/com/example/gymkhana/AttendanceActivity.kt
@@ -13,26 +13,30 @@ class AttendanceActivity : AppCompatActivity() {
 
     private lateinit var calendarRecyclerView: RecyclerView
     private lateinit var calendarAdapter: CalendarAdapter
+    private lateinit var currentDate: Calendar
+    private lateinit var totalDaysTextView: TextView
+    private lateinit var streakLengthTextView: TextView
+    private val highlightedDates = mutableMapOf<Date, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attendance)
-        // Find views
+
         val monthYearTextView: TextView = findViewById(R.id.monthYearTextView)
         val previousMonthButton: Button = findViewById(R.id.previousMonthButton)
         val nextMonthButton: Button = findViewById(R.id.nextMonthButton)
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView)
+        totalDaysTextView = findViewById(R.id.totalDaysTextView)
+        streakLengthTextView = findViewById(R.id.streakLengthTextView)
 
-        // Set up the RecyclerView with the calendar adapter
-        calendarAdapter = CalendarAdapter(generateDatesForMonth())
+        currentDate = Calendar.getInstance()
+        val currentMonthYear = getCurrentMonthYear(currentDate)
+        monthYearTextView.text = currentMonthYear
+
+        calendarAdapter = CalendarAdapter(this, generateDatesForMonth(currentDate), highlightedDates)
         calendarRecyclerView.adapter = calendarAdapter
         calendarRecyclerView.layoutManager = GridLayoutManager(this, 7)
 
-        // Set month-year text
-        val currentMonthYear = getCurrentMonthYear()
-        monthYearTextView.text = currentMonthYear
-
-        // Set click listeners for previous and next month buttons
         previousMonthButton.setOnClickListener {
             navigateToPreviousMonth()
         }
@@ -40,69 +44,99 @@ class AttendanceActivity : AppCompatActivity() {
         nextMonthButton.setOnClickListener {
             navigateToNextMonth()
         }
+
+        updateStreakFeatures()
     }
 
-    private fun generateDatesForMonth(): List<Date> {
-        val calendar = Calendar.getInstance()
+    private fun generateDatesForMonth(calendar: Calendar): List<Date> {
         val dates = mutableListOf<Date>()
 
-        calendar.set(Calendar.DAY_OF_MONTH, 1)
-        val startDate = calendar.time
+        val startCalendar = calendar.clone() as Calendar
+        startCalendar.set(Calendar.DAY_OF_MONTH, 1)
 
-        calendar.add(Calendar.MONTH, 1)
-        calendar.add(Calendar.DATE, -1)
-        val endDate = calendar.time
+        val endCalendar = calendar.clone() as Calendar
+        endCalendar.set(Calendar.DAY_OF_MONTH, endCalendar.getActualMaximum(Calendar.DAY_OF_MONTH))
 
-        calendar.time = startDate
-
-        while (!calendar.time.after(endDate)) {
-            dates.add(calendar.time)
-            calendar.add(Calendar.DATE, 1)
+        while (!startCalendar.after(endCalendar)) {
+            dates.add(startCalendar.time)
+            startCalendar.add(Calendar.DAY_OF_MONTH, 1)
         }
 
         return dates
     }
-    private fun getCurrentMonthYear(): String {
-        val calendar = Calendar.getInstance()
+
+    private fun getCurrentMonthYear(calendar: Calendar): String {
         val dateFormat = SimpleDateFormat("MMMM yyyy", Locale.getDefault())
         return dateFormat.format(calendar.time)
     }
 
     private fun navigateToNextMonth() {
-        // Update the list of dates and notify the adapter
-        val dates = generateDatesForNextMonth()
-        calendarAdapter.updateDates(dates)
-        calendarAdapter.notifyDataSetChanged()
-
-        // Update the month-year text
-        val monthYearTextView: TextView = findViewById(R.id.monthYearTextView)
-        val currentMonthYear = getCurrentMonthYear()
-        monthYearTextView.text = currentMonthYear
+        currentDate.add(Calendar.MONTH, 1)
+        updateCalendar()
+        updateStreakFeatures()
     }
-
-    private fun generateDatesForNextMonth(): List<Date> {
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.MONTH, 1)
-        return generateDatesForMonth(calendar)
-    }
-
 
     private fun navigateToPreviousMonth() {
-        // Update the list of dates and notify the adapter
-        val dates = generateDatesForPreviousMonth()
+        currentDate.add(Calendar.MONTH, -1)
+        updateCalendar()
+        updateStreakFeatures()
+    }
+
+    private fun updateCalendar() {
+        val dates = generateDatesForMonth(currentDate)
         calendarAdapter.updateDates(dates)
         calendarAdapter.notifyDataSetChanged()
 
-        // Update the month-year text
         val monthYearTextView: TextView = findViewById(R.id.monthYearTextView)
-        val currentMonthYear = getCurrentMonthYear()
+        val currentMonthYear = getCurrentMonthYear(currentDate)
         monthYearTextView.text = currentMonthYear
     }
 
-    private fun generateDatesForPreviousMonth(): List<Date> {
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.MONTH, -1)
-        return generateDatesForMonth()
+    private fun updateStreakFeatures() {
+        val dates = generateDatesForMonth(currentDate)
+        val totalDays = calculateTotalDaysPresent(dates)
+        val streakLength = calculateStreakLength(dates)
+        updateTotalDays(totalDays)
+        updateStreakLength(streakLength)
     }
 
+    fun updateTotalDays(totalDays: Int) {
+        totalDaysTextView.text = "Total Days: $totalDays"
+    }
+
+    fun updateStreakLength(streakLength: Int) {
+        streakLengthTextView.text = "Streak Length: $streakLength"
+    }
+
+    private fun calculateTotalDaysPresent(dates: List<Date>): Int {
+        var totalDays = 0
+        for (date in dates) {
+            if (highlightedDates.containsKey(date)) {
+                totalDays++
+            }
+        }
+        return totalDays
+    }
+
+    private fun calculateStreakLength(dates: List<Date>): Int {
+        var streakLength = 0
+        var currentStreak = 0
+
+        for (date in dates) {
+            if (highlightedDates.containsKey(date)) {
+                currentStreak++
+            } else {
+                if (currentStreak > streakLength) {
+                    streakLength = currentStreak
+                }
+                currentStreak = 0
+            }
+        }
+
+        if (currentStreak > streakLength) {
+            streakLength = currentStreak
+        }
+
+        return streakLength
+    }
 }
--- app/src/main/java/com/example/gymkhana/CalendarAdapter.kt
+++ app/src/main/java/com/example/gymkhana/CalendarAdapter.kt
@@ -1,40 +1,100 @@
 package com.example.gymkhana
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import java.text.SimpleDateFormat
+import com.example.gymkhana.AttendanceActivity
 import java.util.*
 
-class CalendarAdapter(var dates: List<Date>) : RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
+class CalendarAdapter(
+    private val context: Context,
+    private var dates: List<Date>,
+    private val highlightedDates: MutableMap<Date, Int>
+) : RecyclerView.Adapter<CalendarAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.calendar_cell, parent, false)
-        return CalendarViewHolder(view)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val dateTextView: TextView = itemView.findViewById(R.id.cellDayText)
     }
 
-    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.calendar_cell, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val date = dates[position]
-        holder.bind(date)
-    }
+        val calendar = Calendar.getInstance()
+        calendar.time = date
 
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        holder.dateTextView.text = dayOfMonth.toString()
+
+        val clickCount = highlightedDates[date] ?: 0
+
+        when (clickCount) {
+            0 -> holder.dateTextView.setBackgroundResource(R.drawable.bg_no_color) // No color
+            1 -> holder.dateTextView.setBackgroundResource(R.drawable.bg_green_color) // Green color
+            2 -> holder.dateTextView.setBackgroundResource(R.drawable.bg_red_color) // Red color
+        }
+
+        holder.itemView.setOnClickListener {
+            val newClickCount = (clickCount + 1) % 3
+            highlightedDates[date] = newClickCount
+            notifyDataSetChanged() // Update the adapter to reflect the changes
+            updateTotalDaysAndStreak()
+        }
+    }
     override fun getItemCount(): Int {
         return dates.size
     }
 
-    fun updateDates(newDates: List<Date>) {
-        dates = newDates
+    private fun toggleDateSelection(date: Date) {
+        if (highlightedDates.containsKey(date)) {
+            highlightedDates.remove(date)
+        } else {
+            highlightedDates[date] = 1
+        }
     }
 
-    inner class CalendarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val dateTextView: TextView = itemView.findViewById(R.id.cellDayText)
+    fun updateDates(newDates: List<Date>) {
+        dates = newDates
+        notifyDataSetChanged()
+    }
 
-        fun bind(date: Date) {
-            val dateFormat = SimpleDateFormat("dd", Locale.getDefault())
-            val dayOfMonth = dateFormat.format(date)
-            dateTextView.text = dayOfMonth
+    private fun updateTotalDaysAndStreak() {
+        val totalDays = highlightedDates.size
+        val streakLength = calculateStreakLength()
+        (context as AttendanceActivity).updateTotalDays(totalDays)
+        (context as AttendanceActivity).updateStreakLength(streakLength)
+    }
+
+    private fun calculateStreakLength(): Int {
+        var streakLength = 0
+        var currentStreak = 0
+
+        val sortedDates = highlightedDates.keys.sorted()
+        val calendar = Calendar.getInstance()
+
+        for (date in sortedDates) {
+            calendar.time = date
+            val dayOfYear = calendar.get(Calendar.DAY_OF_YEAR)
+
+            if (highlightedDates.containsKey(date)) {
+                if (currentStreak == 0 || dayOfYear == currentStreak + 1) {
+                    currentStreak++
+                } else {
+                    currentStreak = 1
+                }
+
+                if (currentStreak > streakLength) {
+                    streakLength = currentStreak
+                }
+            }
         }
+
+        return streakLength
     }
 }
