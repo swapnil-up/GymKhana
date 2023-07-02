b576cca3ad275244d45722be8c839c629d830fb7
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-07-02 19:30:11 +0545

    Starting the admin side. Imported resources and started updated dashboard.

--- gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/MainActivity.kt
+++ gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/MainActivity.kt
@@ -1,21 +1,75 @@
 package com.example.gymkhanaadmin
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imagebtn : ImageButton = findViewById(R.id.adminIcon)
 
-        imagebtn.setOnClickListener(){
-            var i = Intent(this,AdminDetails::class.java)
+        val imagebtn : ImageButton= findViewById(R.id.userIcon)
+        val attendanceButton: Button = findViewById(R.id.attendance)
+        val notificationbtn: Button=findViewById(R.id.notificationbtn)
+        val storeButton:Button=findViewById(R.id.Store)
+        val foodButton :Button= findViewById(R.id.FoodSearch)
+        val mealPlanButton :Button= findViewById(R.id.mealPlan)
+
+
+
+        /*imagebtn.setOnClickListener {
+            var i = Intent(this,UserDetails::class.java)
             startActivity(i)
         }
 
+
+        attendanceButton.setOnClickListener {
+            val intent = Intent(this, AttendanceActivity::class.java)
+            startActivity(intent)
         }
+
+        notificationbtn.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            startActivity(intent)
+        }
+
+        storeButton.setOnClickListener {
+            val intent = Intent(this, StoreActivity::class.java)
+            startActivity(intent)
+        }
+
+        foodButton.setOnClickListener {
+            val intent = Intent(this, FoodSearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        mealPlanButton.setOnClickListener {
+            val intent = Intent(this, MealPlanActivity::class.java)
+            startActivity(intent)
+        }
+        */
+
+        FirebaseApp.initializeApp(this)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val token = task.result
+                Log.d("FCM Token", token ?: "Token retrieval failed")
+            } else {
+                Log.e("FCM Token", "Token retrieval failed", task.exception)
+            }
+        }
+
+    }
+
 }
+
