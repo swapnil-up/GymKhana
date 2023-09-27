817443f25f01f2038f37d4dd1adefbdea6547f3b
manishbajracharya <manish.191419@ncit.edu.np>
2023-09-27 20:41:09 +0545

    added recycler view to UpdateClasses

--- gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/GymClass.kt
+++ gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/GymClass.kt
@@ -1,4 +1,9 @@
 package com.example.gymkhanaadmin.classes
 
-data class GymClass(val className: String, val imageUrl: String)
+data class GymClass(
+    val className: String = "",
+    val classDesc: String = "",
+    val imageUrl: String = ""
+)
+
 
--- gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/UpdateClasses.kt
+++ gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/UpdateClasses.kt
@@ -1,26 +1,81 @@
 package com.example.gymkhanaadmin.classes
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gymkhanaadmin.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
 
 class UpdateClasses : AppCompatActivity() {
+
+    private lateinit var fab: FloatingActionButton
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var classesAdapter: ClassesAdapter
+    private lateinit var databaseRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_classes)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+        // Initialize RecyclerView and its adapter
+        recyclerView = findViewById(R.id.classesList)
+        classesAdapter = ClassesAdapter(emptyList())
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = classesAdapter
 
-        fab.setOnClickListener{
+        fab = findViewById(R.id.fab)
+
+        fab.setOnClickListener {
             Log.d("UpdateClasses", "FloatingActionButton clicked")
             val intent = Intent(this, AddClasses::class.java)
             startActivity(intent)
         }
 
+        // Initialize Firebase Realtime Database reference
+        databaseRef = FirebaseDatabase.getInstance("https://gymkhana-5560f-default-rtdb.asia-southeast1.firebasedatabase.app")
+            .getReference("classes")
+
+        // Load classes data into the adapter
+        loadClassesData()
+    }
+
+    private fun loadClassesData() {
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val classesMap = mutableMapOf<String, GymClass>()
+
+                for (classSnapshot in snapshot.children) {
+                    val key = classSnapshot.key ?: ""
+                    val gymClass = classSnapshot.getValue(GymClass::class.java)
+                    gymClass?.let {
+                        classesMap[key] = it
+                    }
+                }
+
+                // Extract the values from the map to a list
+                val classesList = classesMap.values.toList()
+
+                // Update the RecyclerView adapter with the loaded data
+                classesAdapter.updateData(classesList)
+
+                // Toggle visibility of RecyclerView and "No Data" view
+                if (classesList.isEmpty()) {
+                    recyclerView.visibility = android.view.View.GONE
+                    findViewById<android.view.View>(R.id.clsNoData).visibility = android.view.View.VISIBLE
+                } else {
+                    recyclerView.visibility = android.view.View.VISIBLE
+                    findViewById<android.view.View>(R.id.clsNoData).visibility = android.view.View.GONE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("UpdateClasses", "Error loading classes data: ${error.message}")
+            }
+        })
     }
 
 }
