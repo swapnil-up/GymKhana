0c748ce7d7b7957a585962c8bc5a4def34c4415d
manishbajracharya <manish.191419@ncit.edu.np>
2023-09-27 16:01:22 +0545

    add classes working again

--- gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/UpdateClasses.kt
+++ gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/UpdateClasses.kt
@@ -3,6 +3,7 @@ package com.example.gymkhanaadmin.classes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.example.gymkhanaadmin.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -15,6 +16,7 @@ class UpdateClasses : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener{
+            Log.d("UpdateClasses", "FloatingActionButton clicked")
             val intent = Intent(this, AddClasses::class.java)
             startActivity(intent)
         }
