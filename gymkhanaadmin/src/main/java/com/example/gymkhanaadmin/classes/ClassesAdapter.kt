154c2926f07c028cdd6c5717a7653b4835ec0d2d
swapnil-up <swapnilupadhyaykharel@gmail.com>
2023-09-28 11:31:14 +0545

    Update class now loads images properly. Ali slow bhayera paila placeholder aairaxa though

--- gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/ClassesAdapter.kt
+++ gymkhanaadmin/src/main/java/com/example/gymkhanaadmin/classes/ClassesAdapter.kt
@@ -3,14 +3,18 @@ package com.example.gymkhanaadmin.classes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gymkhanaadmin.R
+import com.squareup.picasso.Picasso
 
 class ClassesAdapter(private var classesList: List<GymClass>) : RecyclerView.Adapter<ClassesAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val classNameTextView: TextView = itemView.findViewById(R.id.className)
+        val classDescription: TextView = itemView.findViewById(R.id.classDescription)
+        val classImageView: ImageView = itemView.findViewById(R.id.classImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,6 +25,14 @@ class ClassesAdapter(private var classesList: List<GymClass>) : RecyclerView.Ada
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val gymClass = classesList[position]
         holder.classNameTextView.text = gymClass.className
+        holder.classDescription.text = gymClass.classDesc
+
+        // Load the image from the URL using Picasso
+         Picasso.get()
+            .load(gymClass.imageUrl)
+            .placeholder(R.drawable.notification) // Placeholder image
+            .error(R.drawable.bg_no_color) // Error image (optional)
+            .into(holder.classImageView)
     }
 
     override fun getItemCount(): Int {
